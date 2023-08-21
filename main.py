f4b379af61427112076ac625afe1d45cba0e07d2
byeongheonYang <byeongheon@gurum.cc>
2023-08-21 16:46:41 +0900

    Add process for output files of multi result

--- main.py
+++ main.py
@@ -17,13 +17,21 @@ perf.proecess_raw_log_file(log_raw_dir,log_temp_dir)
 
 csv_files = [f for f in os.listdir(log_temp_dir) if f.endswith('.csv')]
 
-for csv_file in csv_files: 
-  df = perf.load_data_to_dataframe(log_temp_dir,csv_file)
+for i in range(0,len(csv_files)) : 
+  prefix_path = result_dir + f"/{i}"
+  if not os.path.exists(prefix_path):
+    os.makedirs(prefix_path)
+
+  df = perf.load_data_to_dataframe(log_temp_dir, csv_files[i])
   ax = perf.draw_recv_sent_data(df)
-  plt.savefig(result_dir + "/histogram.png")
+  plt.savefig(prefix_path+"/histogram.png")
+  plt.close()
   ax = perf.draw_CPU_usage(df)
-  plt.savefig(result_dir + "/cpu_usage.png")
+  plt.savefig(prefix_path+"/cpu_usage.png")
+  plt.close()
   ax = perf.draw_troughput(df)
-  plt.savefig(result_dir + '/throunghput.png')
+  plt.savefig(prefix_path + '/throughput.png')
+  plt.close()
   ax = perf.draw_latency_and_memory(df)
-  plt.savefig(result_dir + "/latency_memory.png")
+  plt.savefig(prefix_path+"/latency_memory.png")
+  plt.close()
